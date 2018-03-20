6524793ad23f34d916aedf5e3ceb6b9d24500f83
Karen Jan <karenjan@dhcp-rhodes-2324.eduroam.cornell.edu>
2018-03-20 18:23:27 -0400

    grid_2d update

--- grid_2d.ml
+++ grid_2d.ml
@@ -1,1 +1,20 @@
-type cell = {cell: (int*int*int); row: int; col: int}
+type cell = {cell: (int*int*int); taken: bool; player: int}
+
+module type Plane = sig
+  val positions: cell list
+  val cells_left: cell list -> cell list
+  val three_row_2d: cell -> cell -> cell -> bool
+  val move_valid: cell -> cell list -> bool
+end
+
+module Top: Plane = struct
+  let positions =
+    let tmp_cell = {cell = (0,0,0); row }
+
+  let cells_left lst = failwith "Unimplemented"
+
+  let three_row_2d cell1 cell2 cell3 = failwith "Unimplemented"
+
+  let move_valid cell lst = failwith "Unimplemented"
+
+end
