49fa1a55bc6ef235250eb0c6cb75354f7a8702f2
Karen Jan <karenjan@dhcp-rhodes-2324.eduroam.cornell.edu>
2018-03-20 18:20:25 -0400

    updated grid_2d

--- grid_2d.mli
+++ grid_2d.mli
@@ -12,24 +12,29 @@ val positions : cell list
 
 
 (** [cells_left lst] is a list of the cells that are still free. If no cells are
-  * free, then [cells_left lst] returns []. The list is unordered.
+  * free, then [cells_left lst] returns []. The list is unordered. Used to know
+  * when game is over.
  **)
 val cells_left : cell list -> cell list
 
-(** [three_row_2d cell1 cell2 cell3] is true if the three cells have the same
-  * marker and form either a vertical line, a horizontal line, or a diagonal
-  * line. It is false otherwise. The cells do not have to be in order.
-  * [three_row_2d] will return true so long as a continuous line is formed from
-  * the three cells.
-  * Requires: three valid cells in the same plane.
+(** [cells_occupied lst] is a list of the cells that are occupied. If none of
+  * the cells are occupied, then it returns []. The list is unordered.
+ **)
+val cells_occupied: cell list -> cell list
+
+(** [three_row_2d input_cell lst] is true if the inputted cell makes a vertical,
+  * diagonal, or horizontal line with the same players. It is false otherwise.
+  * Requires: lst to be a list of occuppied cells, the cell the player just
+  * placed their marker in
   * Example:
-  *  - let cell1 = {cell = (0,0,2); taken = true; player = caml} in
+  *  - let cell_input = {cell = (0,0,2); taken = true; player = caml} in
   *  - let cell2 = {cell = (0,0,0); taken = true; player = caml} in
   *  - let cell3 = {cell = (0,0,1); taken = true; player - caml} in
-  *  - [three_row_2d cell1 cell2 cell3] returns true
+  *  - let cell_occupied = [cell2, cell3] in
+  *  - [three_row_2d cell_input cell_occupied] returns true
   *  - (it forms a horizontal line in the top plane in the top row)
  **)
-val three_row_2d: cell -> cell -> cell -> bool
+val three_row_2d: cell -> cell list -> bool
 
 (** [move_valid cell lst] is true if the user can play the move they want to
   * play. [cell] is the move the player wishes to make. [lst] is the list of
