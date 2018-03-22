b2970281599f8fbb805cdf501ba73dbcd334f4f4
Caroline Chang <cdc222@cornell.edu>
2018-03-22 16:18:11 -0400

    more 3d mli fixes - still need to be discussed

--- grid_3d.mli
+++ grid_3d.mli
@@ -1,3 +1,10 @@
+(* [get_parent_plane cell] gets the cell list plane that the current [cell] is
+   in. Maybe the return type would be of type elevation, depending on how we
+   hardcode. resolution needed (*FIXME*)
+*)
+
+val get_parent_plane: cell -> cell list (*(? or a variant??) *)
+
 (* [win_evaluation] takes in [cell] and the 3 [cell list]s making up the grid
    space and determines whether the [cell] is part of a newly won 3-in-a-row
    instance.
@@ -31,10 +38,11 @@ val three_row_col_3d: cell list -> cell list -> cell list -> bool
 val three_row_diag_3d:
 *)
 
-(* [accounted_for] keeps track of the number of 3-in-a-row wins of either
+(* [accounted_for cell list] keeps track of the 3-in-a-row wins of either
    X's or O's. It is not dependent on the player's icon in the
    game. The 3-in-a-row wins are kept in a list of triples,
-   where each triple corresponds to a location in the grid.
+   where each triple corresponds to a location in the grid. If a given cell list
+   is already found, then we don't want to count it again.
  *)
   (*added into accounted_for list, list of 3 triples () *)
-  val accounted_for: cell list
+  val accounted_for: cell list -> bool
