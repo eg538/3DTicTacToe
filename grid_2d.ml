be280e57aef108dd62f016d7909c1868b589fea0
Karen Jan <karenjan@dhcp-rhodes-2515.eduroam.cornell.edu>
2018-03-23 19:51:26 -0400

    fixed the errors in 2d.ml

--- grid_2d.ml
+++ grid_2d.ml
@@ -1,28 +1,37 @@
+open State
+
 module type Plane = sig
   type cell
-  val positions: cell list
+  val get_parent_plane: cell -> cell list
   val cells_left: cell list -> cell list
   val three_row_2d: cell -> cell list -> bool
   val move_valid: cell -> cell list -> bool
+  val retrieve_same_plane_match : cell -> cell list
 end
 
-type player = None | Some Python | Some Caml
+type player = None |  Python |  Caml
 
-type cell = {cell: (int*int*int); taken: bool; player: option player}
+type cell = {cell: (int*int*int); taken: bool; player:  player}
 
 
 module Top: Plane = struct
-  include State
 
-  let positions = failwith "Unimplemented"
+  type cell
+  let get_parent_plane = failwith "Unimplemented"
 
-  let cells_left lst =
-    match lst with
-    | [] -> []
-    | 
+  let cells_left lst = failwith "Unimplemented"
 
   let three_row_2d cell1 lst = failwith "Unimplemented"
 
   let move_valid cell lst = failwith "Unimplemented"
 
+  let retrieve_same_plane_match = failwith "Unimplemented"
+
+
+end
+
+module Middle: Plane = struct
+end
+
+module Bottom: Plane = struct
 end
