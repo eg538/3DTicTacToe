d2b6be30d8a8e0d6e06c34b85df7fb5a83832942
Caroline Chang <cdc222@cornell.edu>
2018-03-23 20:20:14 -0400

    reworking grid_3d-grid_2d, discussing with karen

--- grid_3d.ml
+++ grid_3d.ml
@@ -1,19 +1,12 @@
-open grid_2d
+open Grid_2d
 
 (*TODO: does everyone agree that we need to "hardcode" all the positions for every level
   in this .ml file. So far, I've "grouped" the positions as a variant.
 *)
 
-type elevation = Top | Middle | Bottom
+(*type elevation = Top | Middle | Bottom*)
 
-let win_list = ref 0
-
-let get_parent_plane c = (*assumes that the last coordinate determines the elevation *)
-  let thd x,y,z = z in
-  match (thd c) with
-  | 0 -> Bottom
-  | 1 -> Middle
-  | _ -> Top
+(*let win_list = ref 0
 
 let accounted_for instance =
   if (List.mem (instance) win_list) then false
@@ -21,16 +14,13 @@ let accounted_for instance =
     win_list := instance::win_list;
     true
   )
-
+*)
+let diag_check c = failwith "Unimplemented"
+let col_check c = failwith "Unimplemented"
   (*WIP *)
 let win_evaluation c p1 p2 p3 =
-  let plane = get_parent_plane c in
-  if (three_row_2d c plane) then (*check with karen...*)
-    let three_in_row = (retrieve_same_plane_match c)(*TODO: implement retrieve_match in grid2d; returns a cell-list *)
-    (accounted_for three_in_row)
-  else
-    false
+  (three_row_2d c plane) || (diag_check c) || (col_check c)
 
 let cells_occupied p1 p2 p3 = (*is each plane represented as a cell list *)
-  let whole_space = p1 @ p2 @ p3 in
+  let whole_space = List.fold_left (fun a x -> x::a) [] [p1;p2;p3] in
   List.filter (cell.taken = false) whole_space
