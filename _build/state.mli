3f8221d04fe4f3b97d805d43d06f84fbb91bb3df
eg538 <eg538@cornell.edu>
2018-03-29 23:01:22 -0400

    Commented ai.mli

---
+++ _build/state.mli
@@ -0,0 +1,74 @@
+open Command
+type state
+
+(*[init_state info] is the initial state of the game
+ * ass determined by info (TODO: decide what type info will be),
+ * which is composed of information (mode, avatar, level), where
+ *  - mode: single or multiplayer mode
+ *  - avatar: the player(s)'s avatar(s)
+ *  - level: easy, medium or difficult level
+ *)
+val init_state: info -> state
+
+(*[p1score st] is player 1's score when the game is in state [st].
+ * Player 1 is the human player*)
+val p1score: state -> int
+
+(*[p2score st] is player 2's score when the game is in state [st].
+ * Player 2 could either be a second human player or the computer*)
+val p2score: state -> int
+
+(*[curr_player st] is the player whose turn it is to go when game is in
+ * state [st]*)
+val curr_player: state -> string
+
+(*[asciiBoard st] is the ascii string representation of the 3D tictactoe
+ * board when the game is in state [st]
+ * NOTE: for testing before implementation of GUI*)
+val asciiBoard: state -> string
+
+(*[hint st] is the optimal move for the current player of [st] when game is
+ * in state [st]*)
+val hint: state -> string
+
+(*[board st] is information regarding which spots in the board are filled and
+ * with which player's move
+ * TODO: figure out what type this function should return*)
+val board: state -> string
+
+(*[avatars st] is the assignment of the avatars when game is in state in the form
+ * [("player1", player1's avatar); ("player2", player2's avatar)*)
+val avatars: state -> string * plep list
+
+(*[do' c st] is [st'] if executing command [c] in state [st] results
+ * in [st']. The following describe the valid commands and the result
+ * that [do'] should return for each one when applied to a state [st]
+ *  - The “start” (and its alternate "play"), “try”, “place”
+ *    and “restart” return an appropriately updated [st'] as described
+ *    in the charter, if their object is valid in [st]. If not, then [st']
+ *    is equivalent to [st]
+ *      + The object of "start" (and "play") are valid if state is NULL
+ *        and player has entered valid information for mode of play, avatar
+ *        choices, and level.
+ *      + The object of "try" and "place" are valid if state is not NULL and
+ *        the move is to place at a empty spot in the board
+ *      + The object of "restart" is always valid if state is not NULL. If "restart"
+ *        is valid, then calling [do'] on command "restart" and any state [st]
+ *        returns NULL
+ *  - The “quit” command us always possible. This command, along with the
+ *    commands "look", “hint” and “score”, leave the observable state
+ *    unchanged ([st'] is equivalent to [st]).
+ *      + The object "hint" is only valid if there remains an empty spot on
+ *        the board and the game has not terminated (a game is defined to have terminated
+ *        if there are no empty cells left on the board)
+ *      + The object "score" is only valid if it requests the score of a valid player
+ *        (NOTE: This is so that the current player will not only be able to see his own
+ *        score but also the score of the opponent, whether human or computer)
+ *      + The object "look" is only valid before the proper GUI is implemented and for use
+ *        with the text GUI (NOTE: Do we need look if we get to the fancier GUI, since then
+ *        the board is always in display?)
+ * If [c] is not a valid command, [st'] is equivalent to [st]
+ * effects: none. do' shouldn't print or display anything in order to maintain
+ * model-view-controller
+ *)
+val do': command -> state -> state
--- ai.mli
+++ ai.mli
@@ -1,20 +1,29 @@
 (**
-  * [game_tree] is the main engine for the artificial intelligence aspect of the game.
-  * This is mainly accessed when the player type "hint", and then the state of the game is recieved
+  * ai is the main engine for the artificial intelligence aspect of the game.
+  * This is mainly accessed when the player type "hint", and when the state of the game is recieved
   * or when the game is played in 'single player' mode, and depending on the level ('easy', 'medium', or 'hard')
   * the game tree will choose the sub-optimal to optimal position for the next move on the game board and
-  * thus return a new valid state.
-  * requires: a valid state
+  * thus return a new valid state. This will be implemented using Min and Max search and alpha-beta pruning
  **)
 
-type valid_node = {board: cell list -> cell list -> cell list move: int*int*int}
+(*[valid_node] is a node in the game search tree. It contains the move that
+ * generated the board that is also stored at this node*)
+type valid_node = {board: cell list -> cell list -> cell list; move: int*int*int}
+
+(*[tree] is the game search tree. The nodes are of type valid_node. See comments
+ * for valid_node for more details*)
 type tree = Leaf | Node of valid_node * tree * tree
 
+(*[game_tree_generate nd] is the game search tree generated with nd as the head
+ * node of the tree*)
 val game_tree_generate: valid_node -> tree
 
+(*[available_cells st] is the list of cells that are vacant in the 3D game board
+ * that st contains*)
 val available_cells: state -> cell list
 
-(* cell list is generated from available cells *)
+(*[children nd clist] is list of nodes that are generated as a result of applying
+ * the possible actions specified by clist to the board contained in nd*)
 val children: valid_node -> cell list -> valid_node list
 
 (*TBD*)
