231ebaf31ec0a811357ad48bc748c25ecf87a01b
Karen Jan <karenjan@dhcp-gs-3054.eduroam.cornell.edu>
2018-04-09 15:15:50 -0400

    basically finished grid_2d.ml. made some changes to grid_2d.mli with regards to what arguements are needed for certain functions. have not tested yet

--- grid_2d.ml
+++ grid_2d.ml
@@ -2,8 +2,11 @@ open State
 
 module type Plane = sig
   type cell
+  val get_plane : (int*int*int) -> int
   val get_parent_plane: cell -> state -> cell list
   val cells_left: cell list -> cell list
+  val is_taken : cell -> cell list -> bool
+  val cell_valid : cell -> bool
   val three_row_2d: cell -> cell list -> bool
   val move_valid: cell -> cell list -> bool
 end
@@ -16,14 +19,67 @@ type cell = {cell: (int*int*int); taken: bool; player:  player}
 module Top: Plane = struct
 
   type cell
-  let rec get_parent_plane cell_pos st=
-    failwith "Unimplemented"
 
-  let cells_left lst = failwith "Unimplemented"
+  let get_plane cell_pos =
+    match cell_pos with
+    | (pl, _ , _ ) -> pl
 
-  let three_row_2d cell1 lst = failwith "Unimplemented"
+  let rec get_parent_plane cell_pos st =
+    let pl_match = get_plane cell_pos.cell in
+    let cell_list = st.cell_list (* or whatever this data is called in the state record *) in
+    List.filter (fun x -> get_plane x.cell = pl_match ) cell_list
 
-  let move_valid cell lst = failwith "Unimplemented"
+  let cells_left lst =
+   List.filter (fun x -> x.taken = false) lst
+
+  let is_taken cell lst =
+    List.exists (fun x -> x.cell = cell && x.taken = false ) lst
+
+  let cell_valid cell =
+    match cell.cell with
+    | (pl, x, y) -> if pl = 1 && x = 1 && y = 1 then false
+      else if x < 0 || x >=3 then false
+      else if y < 0 || y >=3 then false
+      else true
+
+  let move_valid cell lst =
+    (cell_valid cell) && (is_taken cell lst)
+
+  let get_x cell_loc =
+    match cell_loc with
+    | (_, ex, _) -> ex
+
+  let get_y cell_loc =
+    match cell_loc with
+    | (_, _, why) -> why
+
+  let three_row_2d cell st =
+    let cell_player = cell.player in
+    let cell_ex = get_x cell.cell in
+    let cell_why = get_y cell.cell in
+    let cell_pl = get_plane cell.cell in
+    (* three in a column *)
+    ((List.mem {cell = (cell_pl, cell_ex, cell_why -1); taken = true; player = cell_player} (get_parent_plane cell st)) &&
+     (List.mem {cell = (cell_pl, cell_ex, cell_why + 1); taken = true; player = cell_player} (get_parent_plane cell st)))
+
+    ||
+
+    (* three in a row *)
+    ((List.mem {cell = (cell_pl, cell_ex -1, cell_why); taken = true; player = cell_player} (get_parent_plane cell st)) &&
+    (List.mem {cell = (cell_pl, cell_ex + 1, cell_why); taken = true; player = cell_player} (get_parent_plane cell st)))
+
+    ||
+
+    (* three in a diagonal starting from leftmost corner *)
+    ((List.mem {cell = (cell_pl, cell_ex -1, cell_why -1); taken = true; player = cell_player} (get_parent_plane cell st)) &&
+     (List.mem {cell = (cell_pl, cell_ex + 1, cell_why + 1); taken = true; player = cell_player} (get_parent_plane cell st )))
+
+    ||
+
+    (* three in a diagonal starting from rightmost corner *)
+    ((List.mem {cell = (cell_pl, cell_ex -1, cell_why + 1); taken = true; player = cell_player} (get_parent_plane cell st)) &&
+    (List.mem {cell = (cell_pl, cell_ex + 1, cell_why -1); taken = true; player = cell_player} (get_parent_plane cell st)))
+
 
 
 
--- grid_2d.mli
+++ grid_2d.mli
@@ -1,6 +1,12 @@
 type player = None | Python | Caml
 type cell = {cell: (int*int*int); taken: bool; player: player}
 
+
+(** [get_plane] is an int tht returns the plane of a given cell. It is a helper
+  * function for get_parnet_plane
+ **)
+val get_plane : int*int*int -> int
+
 (** [get_parent_plane] is a list of the cells that make up a specific [cell]'s plane. Because each
   * plane is 3 x 3, the length of this list should be nine. The list is ordered
   * by the row element of the cell tuple (as plane will be the same for all 9
@@ -18,24 +24,14 @@ val get_parent_plane : cell -> state -> cell list
  **)
 val cells_left : cell list -> cell list
 
-(** [cells_occupied lst] is a list of the cells that are occupied. If none of
-  * the cells are occupied, then it returns []. The list is unordered.
+(** [is_taken cell lst] is a bool that is true if the given cell is currently
+  * occuppied and false otherwise. It is a helper function for move_valid
  **)
-val cells_occupied: cell list -> cell list
+val is_taken : cell -> cell list -> bool
 
-(** [three_row_2d input_cell lst] is true if the inputted cell makes a vertical,
-  * diagonal, or horizontal line with the same players. It is false otherwise.
-  * Requires: lst to be a list of occuppied cells, the cell the player just
-  * placed their marker in
-  * Example:
-  *  - let cell_input = {cell = (0,0,2); taken = true; player = caml} in
-  *  - let cell2 = {cell = (0,0,0); taken = true; player = caml} in
-  *  - let cell3 = {cell = (0,0,1); taken = true; player - caml} in
-  *  - let cell_occupied = [cell2, cell3] in
-  *  - [three_row_2d cell_input cell_occupied] returns true
-  *  - (it forms a horizontal line in the top plane in the top row)
- **)
-val three_row_2d: cell -> cell list -> bool
+(** [cell_valid cell] is a bool that is true if the given cell is a valid cell. It is a helper function for move_valid**)
+val cell_valid : cell -> bool
+
 
 (** [move_valid cell lst] is true if the user can play the move they want to
   * play. [cell] is the move the player wishes to make. [lst] is the list of
@@ -46,3 +42,16 @@ val three_row_2d: cell -> cell list -> bool
   *   - taken is true for that cell in [lst]
  **)
 val move_valid: cell -> cell list -> bool
+
+(** [three_row_2d input_cell state] is true if the inputted cell makes a vertical,
+  * diagonal, or horizontal line with the same players. It is false otherwise.
+  * Requires: current state the game is in
+  * Example:
+  *  - let cell_input = {cell = (0,0,2); taken = true; player = caml} in
+  *  - let cell2 = {cell = (0,0,0); taken = true; player = caml} in
+  *  - let cell3 = {cell = (0,0,1); taken = true; player - caml} in
+  *  - let cell_occupied = [cell2, cell3] in
+  *  - [three_row_2d cell_input cell_occupied] returns true
+  *  - (it forms a horizontal line in the top plane in the top row)
+ **)
+val three_row_2d: cell -> state -> bool
