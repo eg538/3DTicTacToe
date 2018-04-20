ef5c28680bfd99c782ada7c4a9e291bbc823ce92
Caroline Chang <cdc222@cornell.edu>
2018-04-20 19:05:16 -0400

    grid_3d complete

--- grid_3d.ml
+++ grid_3d.ml
@@ -1,23 +1,60 @@
 open Grid_2d
+open State
 
-let find_diagonal_cells c grid_space =
-  match c.cell with
-  | 1,y,_ when (y <> 1) -> List.filter (fun i -> (i |> fst)=1) grid_space
-  | x,1,_ when (x <> 1) -> List.filter (fun j -> (j |> snd)=1) grid_space
-  | x,y,_ when (x = 1) && (y = 1) ->
-    begin
-      let set_1 = List.filter (fun a -> (a |> fst)=1) grid_space in
-      let set_2 = List.filter (fun b -> (b |> snd)=1) grid_space in
-      let set_3 = (*diagonals*)
+type WinType3D =
+  | WinV of cell list
+  | WinH of cell list
+  | WinNone
 
-    end (*corners*)
-  | 1,1,_ ->
-let diag_check c grid_space =
+(*grid_space takes in 3 planes *)
+let plane1 = MakePlane(Plane) in
+let plane2 = MakePlane(Plane) in
+let plane3 = MakePlane(Plane) in
 
+
+let grid_space_construction plane1 plane2 plane3 = (*plane p1 -> list of cells *)
+  let gs = plane1.get_parent_plane @ plane2.get_parent_plane in
+  let gs' = plane3.get_parent_plane @ gs in
+  gs'
 
 let thd x = function
   | _,_,y -> y
   | _ -> failwith "Impossible"
+
+let horizontal_3d_group c grid_space =
+  match c.cell with
+  | 0,y,z when (y=z) -> List.filter (fun a -> (a.cell |> snd)=(a.cell |> thd)) grid_space
+  | 0,_,_ -> List.filter
+               (fun a -> a.cell <> (0,0,1) && a.cell <> (0,1,0) && a.cell <> (0,1,2) && a.cell <> (0.2,1)) grid_space
+  | 1,y,z when (y=z) -> List.filter (fun a -> (a.cell |> snd)=(a.cell |> thd)) grid_space
+  | 1,_,_ -> List.filter
+               (fun a -> a.cell <> (1,0,1) && a.cell <> (1,1,0) && a.cell <> (1,1,2) && a.cell <> (1,2,1)) grid_space
+  | 2,y,z when (y=z) -> List.filter (fun a -> (a.cell |> snd)=(a.cell |> thd)) grid_space
+  | _ -> List.filter
+           (fun a -> a.cell <> (2,0,1) && a.cell <> (2,1,0) && a.cell <> (2,1,2) && a.cell <> (2,2,1)) grid_space
+
+let vertical_3d_groups c grid_space =
+  match c.cell with
+  | x,0,z when (x=z) -> List.filter (fun a -> (a.cell |> fst)=(a.cell |> thd)) grid_space
+  | 0,_,_ -> List.filter
+               (fun a -> a.cell <> (0,0,1) && a.cell <> (1,0,0) && a.cell <> (2,0,1) && a.cell <> (1,0,2)) grid_space
+  | x,1,z when (x=z) -> List.filter (fun a -> (a.cell |> fst)=(a.cell |> thd)) grid_space
+  | 1,_,_ -> List.filter
+               (fun a -> a.cell <> (0,0,1) && a.cell <> (1,1,0) && a.cell <> (1,1,2) && a.cell <> (2,1,1)) grid_space
+  | x,2,z when (x=z) -> List.filter (fun a -> (a.cell |> fst)=(a.cell |> thd)) grid_space
+  | _ -> List.filter
+           (fun a -> a.cell <> (0,2,1) && a.cell <> (1,2,0) && a.cell <> (2,2,1) && a.cell <> (1,2,2)) grid_space
+
+let diag_check c grid_space =
+  let diag_h = horizontal_3d_group in
+  let diag_v = vertical_3d_group in
+  let verdict_h = (List.for_all (fun x -> x.taken = true ) diag_h) in
+  let verdict_v = (List.for_all (fun x -> x.taken = true) diag_v) in
+  match (verdict_h, verdict_v) with
+  | true, true -> WinH diag_h, WinV diag_v
+  | true, false -> WinH diag_h, WinNone
+  | false, true -> WinNone, WinV diag_v
+  | false, false -> WinNone, WinNone
 
 let find_vertical_cells c grid_space =
   let f = c.cell |> fst in
@@ -25,13 +62,14 @@ let find_vertical_cells c grid_space =
   List.filter (fun i -> (i.cell |> fst)=f && (i.cell |> snd)=s) grid_space
 
 let col_check c grid_space =
-  let cell_1 = find_vertical_cells c grid_space |> fst in
+  let cell_1 = (find_vertical_cells c grid_space |> fst in
   let cell_2 = find_vertical_cells c grid_space |> snd in
   (cell_1.taken && (cell_2.taken))
 
 let win_evaluation c p1 p2 p3 =
-  let grid_space = List.flatten [p1;p2;p3] in
-  let cases_3d = (diag_check c) || (col_check c grid_space) in
+  let grid_space = p1@p2@p3 in (*3 grids, 3 cell lists *)
+  let diag_check_truth = ((diag_check c grid_space |> fst) = WinNone) && ((diag_check c grid_space |> fst) = WinNone) in
+  let cases_3d = (diag_check_truth) || (col_check c grid_space) in
   let case_1 = (three_row_2d c p2) || (three_row_2d c p3) in
   let case_2 = (three_row_2d c p1) || (three_row_2d c p3) in
   let case_3 = (three_row_2d c p1) || (three_row_2d c p2) in
@@ -43,3 +81,10 @@ let win_evaluation c p1 p2 p3 =
 let cells_occupied p1 p2 p3 = (*is each plane represented as a cell list *)
   let whole_space = List.fold_left (fun a x -> x::a) [] [p1;p2;p3] in
   List.filter (cell.taken = false) whole_space
+
+let get_the_win c current_player p1 p2 p3=
+  if (win_evaluation c p1 p2 p3) then
+    match (diag_check_truth) with
+    | true  -> diag_check
+    | _ -> find_vertical_cells c grid_space
+  else []
