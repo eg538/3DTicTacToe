f8eede4ce5a6ca97aca59f4414066c53edafa2ea
Caroline Chang <cdc222@cornell.edu>
2018-04-26 22:25:49 -0400

    ai implementation start

---
+++ ai.ml
@@ -0,0 +1,44 @@
+open State
+
+type valid_node = {board: cell list -> cell list -> cell list; mutable move: int*int*int}
+type tree = Leaf | Node of valid_node * tree * tree
+
+let game_tree_generate valid_node st =
+  let remaining_cells = available_cells st in
+
+(*[available_cells st] is the list of cells that are vacant in the 3D game board
+ * that st contains*)
+let available_cells st =
+  let positions = [(0,0,0); (0,0,1);(0,0,2);(0,1,0);(0,1,1);(0,1,2);(0,2,0);(0,2,1);
+                   (0,2,2);(1,0,0);(1,0,1);(1,0,2);(1,1,0);(1,1,2);(1,2,0);(1,2,1);
+                   (1,2,2);(2,0,0);(2,0,1);(2,0,2);(2,1,0);(2,1,1);(2,1,2);(2,2,0);
+                   (2,2,1);(2,2,2)] in
+  List.filter (fun x -> let temp = (Hashtbl.find (st.cells) x) in temp.player = None) positions
+
+let children nd clist =
+  failwith "unimplemented"
+
+(*[heuristic_fxn valid_node] calculates the utility value based on a yet to be
+  determined heuristic function from [valid_node]'s perspective in the game
+*)
+  let heuristic_fxn nd =
+    failwith "unimplemented"
+
+(* [alpha_beta_pruned_tree tree] returns a [tree] with the removed game states
+   that don't necessarily need to be checked to find the optimal move. utility
+   values are determined based on the heuristic function
+*)
+let alpha_beta_pruned_tree t =
+  failwith "unimplemented"
+
+(* [player_hint tree] returns the optimal [cell] that should be played by the
+   current player to maximize chances of winning as the player
+*)
+let player_hint t =
+  failwith "unimplemented"
+
+(* [computer_turn tree] returns the optimal [cell] that should be played by the
+   computer to maximize chances of winning as the computer
+*)
+let computer_turn t =
+  failwith "unimplemented"
