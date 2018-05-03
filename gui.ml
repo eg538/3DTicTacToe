14ed3f47052807c34839df377cdbbdd97c8d3510
Sheetal Athrey <sheetalathrey@dhcp-rhodes-2092.eduroam.cornell.edu>
2018-05-03 12:57:00 -0400

    just adding background pic for 2nd screen code

--- gui.ml
+++ gui.ml
@@ -84,7 +84,7 @@ let init_welcome f =
   let mouse_status = wait_next_event event_lst  in
   let x = mouse_status.mouse_x in
   let y = mouse_status.mouse_y in
-  if (x >= 420 && x <= 618) && (y >= 20 && y <= 89) then (clear_graph(); draw_image (get_img "imgs/mainTTT.jpg") 220 40;)
+  if (x >= 420 && x <= 618) && (y >= 20 && y <= 89) then (clear_graph(); draw_image (get_img "imgs/tttbgg.jpg") 0 0;draw_image (get_img "imgs/mainTTT.jpg") 220 40;)
        else draw_rect 250 150 90 90;
 
   if (x >= easy_x_min && x <= easy_x_max) && (y >= easy_y_min && y <= easy_y_max) then (draw_rect easy_x_min easy_y_max 90 90)
