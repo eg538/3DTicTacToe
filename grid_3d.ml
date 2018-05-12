2af0135dfbf236c42f5782456511b500a46136f1
eg538 <eg538@cornell.edu>
2018-05-12 11:24:16 -0400

    Added separate module for krazy mode

--- grid_3d.ml
+++ grid_3d.ml
@@ -333,21 +333,3 @@ let get_all_win_inst st c =
 let extract_cell_pos inst =
   List.map (fun x -> x.cell) inst
 
-let krazy_recalc_score st b =
-  let win_inst_tracker = [] in
-  let occupied = cells_occupied b in
-  List.map (fun x -> get_all_win_inst st x) occupied
-  (*traverse through 2d *)
-  (*traverse through 3d*)
-
-let krazy_mode_bomb st c =
-  let b = st.tttBoard in
-  let col_3d = find_vertical_cells c b in (*cell list list*)
-  let v_3d_diag = List.flatten (vertical_3d_groups c b) in (*cell list list*)
-  let h_3d_diag = List.flatten (horizontal_3d_group c b) in (*cell list list*)
-  let plane_2d_inst = List.flatten (three_row_2d_cells c b) in (*cell list list *)
-  let instances = col_3d @ v_3d_diag @ h_3d_diag @ plane_2d_inst in
-  let inst_list = extract_cell_pos instances in
-  let b' = List.iter (fun i ->
-      Hashtbl.replace b (Hashtbl.find b i).cell {(Hashtbl.find b i) with player=None}) inst_list in
-  st (*tttBoard = b'*)
