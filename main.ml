f934e4fcb405630999dda7a49ac20c938221fec3
Sheetal Athrey <sheetalathrey@dhcp-rhodes-2092.eduroam.cornell.edu>
2018-05-12 12:20:20 -0400

    soem hacky fixes, some not so hacky! Gui doesnt shut down, everything seems G G

--- main.ml
+++ main.ml
@@ -9,6 +9,7 @@ open Gui
 
 exception Terminated
 exception Restart
+exception GameEnd
 
 (*[string_of_player p] is the string representation of [p]*)
 let string_of_player p = match p with
@@ -16,9 +17,24 @@ let string_of_player p = match p with
   | Caml -> "Caml"
   | None -> "None"
 
+              (* if game_ended newSt then
+                (
+                  let win_msg = get_result_message newSt in
+                  print_endline win_msg;
+                  play newSt
+                )
+                (*raise GameEnd*)
+              else *)
+
 (*[play st] is the helper function for play_game ()*)
 let rec play st=
-  print_endline "Please enter command";
+  if game_ended st then
+    (
+      let win_msg = get_result_message st in
+      print_endline win_msg
+    )
+  else
+  (print_endline "Please enter command";
   let playerr = match (State.curr_player st ) with
     | Caml -> "caml"
     | Python -> "python"
@@ -29,47 +45,44 @@ let rec play st=
   let com = fst test in
   let command = parse com in
   let newSt = do' command st in
-  if game_ended newSt then
-    let win_msg = get_result_message newSt in
-    print_endline win_msg;
-    (* print_endline (get_result_message newSt) *)
-  else
-  (*Remember to check for win*)
-    match command with
-    | Play str -> print_endline "A game is currently is session. Please quit first.";
-      play newSt
-    | Score ->
-      print_endline ("Score of player 1: "^(string_of_int (p1_score st))^"\n"^"Score of player 2: "^(string_of_int (p2_score st)));
-      play newSt
-    | Quit -> raise Terminated
-    | Restart -> raise Restart
-    | Try (pl, x, y) -> failwith "Unimplemented"
-    | Place (pl, x, y) ->
-      if newSt = st then
-        let ex = snd test |> fst in
-        let why = snd test |> snd in
-        print_int ex;
-        print_int why;
-        (Gui.repeat_cell ex why;
-        (print_endline "Action impossible. Please try a different move.";
-         play newSt);)
-      else
-        print_board newSt;
-      let x = snd test |> fst in
-      let y = snd test |> snd in
-      Gui.cover_up ();
-      print_int x;
-      print_int y;
-        Gui.responsive_board playerr x y ;
-        print_endline ("Score of player 1: "^(string_of_int (p1_score newSt))^"\n"^"Score of player 2: "^(string_of_int (p2_score newSt)));
-        play newSt
-    | Hint -> failwith "Unimplemented"
-    | Look -> print_board st; play newSt
-    | CurrentPlayer ->
-      print_endline ("Current player: "^(string_of_player (curr_player st)));
-      play newSt
-    | Invalid -> print_endline "Action impossible. Please try a different move.";
-      play newSt
+    (*Remember to check for win*)
+  print_endline "am i here?" ;
+  match command with
+  | Play str -> (print_endline "A game is currently is session. Please quit first.";
+                 play newSt)
+  | Score ->
+    (print_endline ("Score of player 1: "^(string_of_int (p1_score st))^"\n"^"Score of player 2: "^(string_of_int (p2_score st)));
+     play newSt)
+  | Quit -> (print_endline "yo what's up in this hole";exit 0)
+  | Restart -> (raise Restart)
+  | Try (pl, x, y) -> (failwith "Unimplemented")
+  | Place (pl, x, y) ->
+    (if newSt = st then
+      (let ex = snd test |> fst in
+      let why = snd test |> snd in
+      print_int ex;
+      print_int why;
+      Gui.repeat_cell ex why;
+      print_endline "Action impossible. Please try a different move.";
+       play newSt;)
+    else
+      (  print_board newSt;
+    let x = snd test |> fst in
+    let y = snd test |> snd in
+    Gui.cover_up ();
+    print_int x;
+    print_int y;
+      Gui.responsive_board playerr x y ;
+      print_endline ("Score of player 1: "^(string_of_int (p1_score newSt))^"\n"^"Score of player 2: "^(string_of_int (p2_score newSt)));
+    play newSt))
+  | Hint -> (failwith "Unimplemented")
+  | Look -> (print_board st; play newSt)
+  | CurrentPlayer ->
+    (print_endline ("Current player: "^(string_of_player (curr_player st)));
+     play newSt)
+  | Invalid -> (print_endline "Action impossible. Please try a different move.";
+                play newSt)
+  )
 
 let rec play_game str f =
 try (
@@ -85,14 +98,15 @@ try (
       play init_st
       ) with
     | Terminated -> print_endline "Bye!"
-    | Restart -> print_endline "You have chosen to restart this game";
-        f ()
+    | Restart -> (print_endline "You have chosen to restart this game";
+                  f ())
+    | GameEnd -> print_endline "Game End"
     | _ -> print_endline "Error"
     end
   | _ -> print_endline "Invalid command. No ongoing game."
   end
 ) with
-| _ -> print_endline "Invalid"
+| _ -> (print_endline "Invalid")
 
 let rec main () =
   ANSITerminal.(print_string [red]
